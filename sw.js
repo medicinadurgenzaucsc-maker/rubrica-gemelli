b1b295f014a3efaa3347d07c17fc3a5efef0c027
medicinadurgenzaucsc-maker <medicinadurgenza.ucsc@gmail.com>
2026-04-25 01:07:31 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'rubrica-v1';
+const STATIC = ['./', './index.html', './manifest.json', './sw.js', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('script.google.com')) {
+    e.respondWith(
+      fetch(e.request).catch(() =>
+        new Response('{"error":"offline"}', { headers: { 'Content-Type': 'application/json' } })
+      )
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
